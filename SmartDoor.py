8cf3df020794da34494a08868a24df1bc041cbf8
Adam Caviness <adamcaviness@gmail.com>
2014-10-21 00:55:07 -0400

    Now using a config file and modified gitignore

--- SmartDoor.py
+++ SmartDoor.py
@@ -1,11 +1,19 @@
 import os
 import re
+import sys
+import json
 import picamera
 import datetime
 from pushbullet import PushBullet
 
 # Implements pushbullet.py, see https://pypi.python.org/pypi/pushbullet.py
 # Implements picamera.py, see http://raspberrypi.org/picamera-pure-python-interface-for-camera-module
+
+executingPath = os.path.dirname(sys.argv[0])
+configFileName = os.path.join(executingPath, "config.json")
+    
+with open(configFileName) as conf:
+    config = json.load(conf)
 
 fileName = datetime.datetime.now().strftime("%I:%M%p %m-%d-%Y") + ".jpg"
 filePath = os.path.join("/home/pi", fileName)
@@ -16,7 +24,7 @@ with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     camera.capture(filePath)
 
-pb = PushBullet("v1N0D2jWlOZKQD6euD4C8F6z24ftoGXtMcujBXUzpQPp6")
+pb = PushBullet(config["auth_key"])
 
 with open(filePath, "rb") as pic:
     message = "SmartDoor " + re.sub(".png", "", fileName)
@@ -26,3 +34,5 @@ print("Success uploading picture " + file_data.get("file_name") + " at url " + f
 success, push = pb.push_file(**file_data)
 print(push.get("iden") + " succeeded")
 os.remove(filePath)
+
+
